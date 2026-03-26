8c76a2eca3bf83fd04bc5587f540ed4b32c7c603
Tobias Andersen <turbolego@gmail.com>
2026-03-26 15:39:19 +0100

    Fix syntaxHighlight import statement

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ const { DateTime } = require("luxon");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
 const CleanCSS = require("clean-css");
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight").default;
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const htmlminifier = require("html-minifier-terser");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 
